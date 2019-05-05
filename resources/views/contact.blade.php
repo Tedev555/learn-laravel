8f3a865619fd7f3b6deb0a3c90f23b5efb9d6825
ted555 <thanong984@gmail.com>
2019-05-05 21:40:53 +0700

    Lesson 03: Basic Routing

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,10 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome Laravel</title>
+    <title>Contact</title>
 </head>
 <body>
-    <h>Welcome to learning Laravel.</h>
-    <p><a href="/learn-laravel/public/contact">Contact</a> us to learn more.</p>
+    <h>Hello!, from contact page.</h>
 </body>
 </html>
