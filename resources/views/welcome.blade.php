765c49ee4f0e7a54ed9557880866cad29b24fdb6
ted555 <thanong984@gmail.com>
2019-05-06 09:01:32 +0700

    Lesson 04: Blade Layout Files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome Laravel</title>
-</head>
-<body>
-    <h>Welcome to learning Laravel.</h>
-    <p><a href="/learn-laravel/public/contact">Contact</a> us to learn more.</p>
-</body>
-</html>
+@extends('layout')
+
+@section('title', 'Learning Laravel')
+
+@section('content')
+    <h1>My first website</h1>
+@endsection
